cfc3532a87412a9ee512c318bd0949cf4d9474e7
dkxkg <dkxkg@DESKTOP-6ND9QD0>
2020-11-02 19:28:46 +0900

    11021928

---
+++ 0914test/src/알고리즘1031/JavaPractice.java
@@ -0,0 +1,96 @@
+package 알고리즘1031;
+
+public class JavaPractice {
+
+	public static void main(String[] args) {
+//해결찝찝하게
+//		int a[] = new int [5];
+//		int i ;
+//		for (i=0; i<5; i++) {
+//			a[i] = i+10;
+//	
+//	       for(i=0; i<5; i++) 
+//	 			
+//				System.out.printf("%4d",a[i]);
+		
+//=================================================================	
+//		//문자열 거꾸로 출력
+//		String str = "Information!";
+//		int n =str.length();
+//		System.out.println(str.length());
+//		char [] st = new char[n];
+//		n--;
+//		for(int k = n; k >=0; k--) {
+//			st[n-k] = str.charAt(k);//charAt문장중에 인덱스 위치에 해당되는 문자 추출
+		//이해잘안감
+//			System.out.println(st[n-k] = str.charAt(k));
+//		}
+//			for(char k : st) {
+//				System.out.println(k);
+//			}
+		
+//==========================================================================
+	//이부분도 이해가안갔지만 해결
+//		int [] [] a = {{11,12,13,14},{21,22,23,24}};
+//		int hap =0;
+//		for(int i[] :a) {
+//			System.out.println();
+//			
+//			for(int j: i) {
+//				System.out.println(hap);
+//				hap = hap +j;
+//			}
+//		}
+//		System.out.println(hap);
+//		==========================================
+//		String str ="Programming";
+//		int n = str.length();
+//		char[] st = new char[n];
+//		n--;
+//		for (int k = n; k >= 0; k--) {
+//			st[n-k] = str.charAt(k);
+//		}
+//		for (char k : st) {
+//			System.out.printf("%c",k);
+//		}
+//=======================================
+		//이해안갔지만 해결했음 
+//		int [][] a = new int [3][5];
+//		
+//		for(int i=0; i<3; i++) {
+//			for(int j =0; j<5; j++) {
+//				 a[i][j]= i+j;
+//				// System.out.println(a[i][j]);
+//				// System.out.println("i"+i);
+//				// System.out.println("j"+j);
+//				//System.out.println("i+j"+i+j);
+//				// System.out.println("a[0][1]"+a[0][1]);
+//				 System.out.printf("%d",a[i][j]);
+//			}
+//		System.out.println();
+//		}
+		//=====================================
+//		int i,j =0;
+//		for (i =0;i<8; i++) {
+//			j+=i;
+//			System.out.println(j);
+//		}
+//		System.out.printf("%d,%d\n",i,j);
+	//	==========================================
+		
+//		for(int i=2; i<=9; i++) {
+//			for(int j=1; j<=9; j++) {
+//				if(j*i==18 ) {
+//					System.out.println("출력");
+//				}
+//				
+//				System.out.println(i+"x"+j+"="+i*j);
+//				
+//				}
+//				
+//			
+//		}
+		
+	}
+          
+}
