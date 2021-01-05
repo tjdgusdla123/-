891a5bcd0f7a70287b669955da2794743dd6422b
dkxkg <dkxkg@DESKTOP-6ND9QD0>
2021-01-06 08:45:48 +0900

    202101060845

--- 0914test/src/알고리즘1031/JavaPractice.java
+++ 0914test/src/알고리즘1031/JavaPractice.java
@@ -282,6 +282,26 @@ public class JavaPractice {
 //				break;
 //			}
 //		}
+	//===============================================
+		//20210106 10진수를 입력받아 2진수로 변환하기
+//		int b = 30,bb,c,mok,nmg,i;
+//		int a[] = new int [10];
+//       
+//		bb=b;
+//		c= -1;
+//		do {
+//		 
+//			c++;
+//			mok = b/2;
+//			nmg = b -mok *2 ;
+//			a[c] = nmg;
+//			b=mok;
+//			
+//		}while(mok != 0);
+//		System.out.println(bb);
+//		for(i = c; i >=0; i--)
+//		System.out.println(a[i]);
+//			
 	}
-	
-	}
+
+}
